cf4e32d2f4a12d82399f9bba83dc5053b415c41c
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-08-28 13:11:13 +0700

    filter sort map

---
+++ src/Components/LowCalDesserts.js
@@ -0,0 +1,28 @@
+import desserts from '../Database/desserts';
+
+function LowCalDesserts() {
+  return (
+    <div className='low-cal-desserts'>
+      <h2>List of low calorie desserts:</h2>
+      <DessertsList data={desserts} />
+    </div>
+  );
+}
+
+export default LowCalDesserts;
+
+function DessertsList(props) {
+  const ListItems = props.data
+    .filter((dessert) => dessert.calories < 500)
+    .sort((a, b) => a.calories - b.calories)
+    .map((dessert) => {
+      const itemsName = `${dessert.name} - ${dessert.calories}`;
+      return <li>{itemsName} Calories</li>;
+    });
+
+  return (
+    <div>
+      <ul>{ListItems}</ul>
+    </div>
+  );
+}
