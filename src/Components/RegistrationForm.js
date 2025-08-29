117f11989cf094192a7e8df14e12eb20e4453869
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-08-29 12:00:39 +0700

    attempt 1

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -1,32 +1,52 @@
 import { useState } from 'react';
 import { validateEmail } from '../utils';
 
-const PasswordErrorMessage = () => {
-  return (
-    <p className='FieldError'>Password should have at least 8 characters</p>
-  );
+const PasswordErrorMessage = (password) => {
+  if (password.value.length < 8) {
+    return (
+      <p className='FieldError'>Password should have at least 8 characters</p>
+    );
+  }
 };
 
 export const RegistrationForm = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
+
   const [password, setPassword] = useState({
     value: '',
     isTouched: false,
   });
+
   const [role, setRole] = useState('role');
 
+  // const invalidForm = () => {
+  //   return <p className='FieldError'>Please fill in all the required fields</p>;
+  // };
+
   const getIsFormValid = () => {
-    // Implement this function
-    return true;
+    if (
+      firstName === '' ||
+      email === '' ||
+      validateEmail(email) ||
+      password.value.length >= 8 ||
+      role === 'role'
+    ) {
+      return true;
+    }
   };
 
   const clearForm = () => {
-    // Implement this function
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setPassword({ value: '', isTouched: false });
+    setRole('role');
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     alert('Account created!');
     clearForm();
   };
@@ -43,23 +63,44 @@ export const RegistrationForm = () => {
             <label>
               First name <sup>*</sup>
             </label>
-            <input placeholder='First name' />
+            <input
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder='First name'
+            />
           </div>
           <div className='Field'>
             <label>Last name</label>
-            <input placeholder='Last name' />
+            <input
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder='Last name'
+            />
           </div>
           <div className='Field'>
             <label>
               Email address <sup>*</sup>
             </label>
-            <input placeholder='Email address' />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Email address'
+            />
           </div>
           <div className='Field'>
             <label>
               Password <sup>*</sup>
             </label>
-            <input placeholder='Password' />
+            <input
+              value={password.value}
+              onChange={(e) =>
+                setPassword({ value: e.target.value, isTouched: true })
+              }
+              placeholder='Password'
+              onFocus={(e) => (e.target.value = password.value)}
+              onBlur={(e) => (e.target.value = '********')}
+            />
+            {password.isTouched && PasswordErrorMessage(password)}
           </div>
           <div className='Field'>
             <label>
