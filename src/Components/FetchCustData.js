db4fbbb8673195aa18ed4fe2671c22ace3184eaf
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-08-31 21:51:01 +0700

    fetchCustData attempt 1

--- src/Components/FetchCustData.js
+++ src/Components/FetchCustData.js
@@ -3,7 +3,13 @@ import React from 'react';
 function FetchCustData() {
   const [user, setUser] = React.useState([]);
 
-  const fetchData = () => {};
+  const fetchData = () => {
+    fetch('https://randomuser.me/api/?results=1').then((response) =>
+      response.json().then((data) => {
+        setUser(data.results[0]);
+      })
+    );
+  };
 
   React.useEffect(() => {
     fetchData();
@@ -13,6 +19,8 @@ function FetchCustData() {
     <div className='section'>
       <h1>Fetch Customer data</h1>
       <h2>Customer data</h2>
+      <h3> Name:{user.results[0].name.first}</h3>
+      <img src={user.results[0].picture.large} alt='' />
     </div>
   ) : (
     <h1>Data pending...</h1>
