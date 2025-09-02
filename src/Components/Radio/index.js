14f91d51b6e71520c18f797bd68adb1dd4f403f5
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-03 00:07:41 +0700

    done react clone

--- src/Components/Radio/index.js
+++ src/Components/Radio/index.js
@@ -4,7 +4,12 @@ import './styles.css';
 export const RadioGroup = ({ onChange, selected, children }) => {
   // Use React.Children.map and React.cloneElement to clone the children
   // and pass the correct props to each RadioOption
-  const RadioOptions = null;
+  const RadioOptions = React.Children.map(children, (child) => {
+    return React.cloneElement(child, {
+      onChange,
+      checked: child.props.value === selected,
+    });
+  });
 
   return <div className='RadioGroup'>{RadioOptions}</div>;
 };
@@ -12,9 +17,36 @@ export const RadioGroup = ({ onChange, selected, children }) => {
 export const RadioOption = ({ value, checked, onChange, children }) => {
   // Hook up the onChange handler to call the onChange prop passed to RadioGroup
   // Also, make sure to pass the correct checked prop to the input element
+  // const [wasChecked, setWasChecked] = React.useState('');
+
+  // const handleChange = (e) => {
+  //   if (checked && wasChecked) {
+  //     if (onChange) onChange(null);
+  //     setWasChecked(false);
+  //   } else {
+  //     if (onChange) onChange(e.target.value);
+  //     setWasChecked(true);
+  //   }
+  // };
+
+  // if (!checked && wasChecked) {
+  //   setWasChecked(false);
+  // }
+
+  const handleChange = (e) => {
+    onChange(e.target.value);
+  };
   return (
     <div className='RadioOption'>
-      <input id={value} type='radio' name={value} />
+      <input
+        type='radio'
+        id={value}
+        name={value}
+        value={value}
+        checked={checked}
+        onChange={handleChange}
+        readOnly
+      />
       <label htmlFor={value}>{children}</label>
     </div>
   );
