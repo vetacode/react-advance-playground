8698ff50aa938a6b0254ff65f25c87d45fe49432
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-07 18:13:18 +0700

    minor change on theme

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -1,9 +1,10 @@
 import { createContext, useContext, useState } from 'react';
 
-const ThemeContext = createContext('light');
+const ThemeContext = createContext(undefined);
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
+  // const toggleTheme = () => setTheme(!theme); //wrong
   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
